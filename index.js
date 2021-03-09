cd37a8876b3f52d869186e9b789fef20e13fe625
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-03-09 17:28:02 +0530

    GET all members information

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const app = express();
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Members data
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
+// API Endpoints - GET all members information
+app.get('/api/members', (req, res) => {
+    res.json(members);
+})
+
+// For Listening Server 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
