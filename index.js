761ed22829da39b69bb455228b8a475cb8f99d23
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-03-09 17:39:44 +0530

    Refactored members data to new file

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
 
 const app = express();
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Members data
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Bob Williams',
-        email: 'bob@gmail.com',
-        status: 'inactive'
-    },
-    {
-        id: 3,
-        name: 'Shannon Jackson',
-        email: 'shannon@gmail.com',
-        status: 'active'
-    }
-];
 
 // API Endpoints - GET all members information
 app.get('/api/members', (req, res) => {
