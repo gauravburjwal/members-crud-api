aa5bbe0319b1c70f22b8085530459c7f94ab708a
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-03-09 18:44:15 +0530

    Implemented GET single member with id

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
-const members = require('./Members');
-const logger = require('./middleware/logger');
+const express = require("express");
+const path = require("path");
+const members = require("./Members");
+const logger = require("./middleware/logger");
 
 const app = express();
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Init middleware
 app.use(logger);
@@ -14,8 +14,20 @@ app.use(logger);
 // API Endpoints
 
 // GET all members information
-app.get('/api/members', (req, res) => {
+app.get("/api/members", (req, res) => {
     res.json(members);
+});
+
+// GET single member
+app.get("/api/members/:id", (req, res) => {
+    // console.log(req.params.id);
+    const found = members.some((member) => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ msg: `No member with id of ${req.params.id}` });
+    }
 });
 
 // For Listening Server
